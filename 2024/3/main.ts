6cf4b9879004eba959da29d8ea77fecf18e2bc53
Bash Forgo <6547523+bashforgo@users.noreply.github.com>
2024-12-04 09:16:36 +0000

    day 3

---
+++ 2024/3/main.ts
@@ -0,0 +1,33 @@
+import { sumOf } from "@std/collections";
+import { getInput } from "@utilities/getInput.ts";
+
+const input = await getInput(3);
+
+const mulExpressionRegex = /mul\((\d{1,3}),(\d{1,3})\)/;
+const doExpressionRegex = /do\(\)/;
+const doNotExpressionRegex = /don't\(\)/;
+const expressionRegex = new RegExp(
+  `(${mulExpressionRegex.source}|${doExpressionRegex.source}|${doNotExpressionRegex.source})`,
+  "g",
+);
+
+const expressions = input.match(expressionRegex)!;
+
+let isEnabled = true;
+const enabledMulExpressions = expressions.filter((expression) => {
+  if (expression.match(doNotExpressionRegex)) {
+    isEnabled = false;
+  }
+
+  if (expression.match(doExpressionRegex)) {
+    isEnabled = true;
+  }
+
+  return isEnabled && expression.match(mulExpressionRegex);
+});
+
+const sum = sumOf(enabledMulExpressions, (expression) => {
+  const [, a, b] = expression.match(mulExpressionRegex)!;
+  return Number(a) * Number(b);
+});
+console.log(sum);
