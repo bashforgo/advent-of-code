1c164f64a44896aeaa778c40b30e4da25ced9b72
Bash Forgo <6547523+bashforgo@users.noreply.github.com>
2024-12-26 14:43:11 +0000

    2015 day 2

---
+++ 2015/02/main.ts
@@ -0,0 +1,30 @@
+import { minBy, sumOf } from "@std/collections";
+import { getInput } from "@utilities/getInput.ts";
+
+const input = await getInput(2015, 2);
+const boxes = input.trim().split("\n").map((line) => {
+  const [length, width, height] = line.split("x").map(Number);
+  return { length, width, height };
+});
+
+const part1 = () => {
+  return sumOf(boxes, ({ length, width, height }) => {
+    const sides = [length * width, width * height, height * length];
+    const smallestSide = minBy(sides, (x) => x)!;
+    return sumOf(sides, (x) => 2 * x) + smallestSide;
+  });
+};
+console.log(part1());
+
+const part2 = () => {
+  return sumOf(boxes, ({ length, width, height }) => {
+    const [shortestPerimeter] = [
+      2 * (length + width),
+      2 * (width + height),
+      2 * (height + length),
+    ].sort((a, b) => a - b);
+    const volume = length * width * height;
+    return shortestPerimeter + volume;
+  });
+};
+console.log(part2());
