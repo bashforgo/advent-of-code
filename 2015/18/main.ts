b9547a138e6230ef9cab7a6f01334f722d993543
Bash Forgo <6547523+bashforgo@users.noreply.github.com>
2025-02-03 10:44:05 +0000

    2015 day 18

---
+++ 2015/18/main.ts
@@ -0,0 +1,88 @@
+import { getInput } from "@utilities/getInput.ts";
+import { addPoints } from "@utilities/grid/addPoints.ts";
+import { getPoint } from "@utilities/grid/getPoint.ts";
+import { Grid } from "@utilities/grid/Grid.ts";
+import { Point, point } from "@utilities/grid/Point.ts";
+
+const DEBUG = false;
+const input = DEBUG
+  ? `\
+.#.#.#
+...##.
+#....#
+..#...
+#.#..#
+####..
+`
+  : await getInput(2015, 18);
+
+const lines = input.trim().split("\n");
+const lights: Grid<boolean> = lines.map((line) =>
+  line.split("").map((c) => c === "#")
+);
+
+const getNeighborStates = (lights: Grid<boolean>, p: Point) => {
+  const deltas = [
+    point(-1, 0),
+    point(-1, 1),
+    point(0, 1),
+    point(1, 1),
+    point(1, 0),
+    point(1, -1),
+    point(0, -1),
+    point(-1, -1),
+  ];
+
+  return deltas.map((delta) => getPoint(lights, addPoints(p, delta)) ?? false);
+};
+
+const step = (lights: Grid<boolean>) => {
+  return lights.map((row, y) =>
+    row.map((light, x) => {
+      const neighborStates = getNeighborStates(lights, point(x, y));
+      const numberOfNeighborsOn = neighborStates.filter((s) => s).length;
+
+      if (light) {
+        const shouldStayOn = numberOfNeighborsOn === 2 ||
+          numberOfNeighborsOn === 3;
+        return shouldStayOn;
+      } else {
+        const shouldTurnOn = numberOfNeighborsOn === 3;
+        return shouldTurnOn;
+      }
+    })
+  );
+};
+
+// deno-lint-ignore no-unused-vars
+const printLights = (lights: Grid<boolean>) => {
+  console.log(
+    lights.map((row) => row.map((l) => (l ? "#" : ".")).join("")).join("\n"),
+  );
+};
+
+const part1 = () => {
+  const numberOfSteps = DEBUG ? 4 : 100;
+  let currentLights = lights;
+  for (let i = 0; i < numberOfSteps; i++) {
+    currentLights = step(currentLights);
+  }
+  return currentLights.flat().filter((l) => l).length;
+};
+console.log(part1());
+
+const turnOnCorners = (lights: Grid<boolean>) => {
+  return lights
+    .with(0, lights.at(0)!.with(0, true).with(-1, true))
+    .with(-1, lights.at(-1)!.with(0, true).with(-1, true));
+};
+
+const part2 = () => {
+  const numberOfSteps = DEBUG ? 5 : 100;
+  let currentLights = turnOnCorners(lights);
+  for (let i = 0; i < numberOfSteps; i++) {
+    currentLights = turnOnCorners(step(currentLights));
+  }
+  return currentLights.flat().filter((l) => l).length;
+};
+console.log(part2());
