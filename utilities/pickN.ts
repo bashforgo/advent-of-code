0f2028ca912504245c8323604c92253bbc845522
Bash Forgo <6547523+bashforgo@users.noreply.github.com>
2025-02-08 09:23:58 +0000

    separate test from implementation

--- utilities/pickN.ts
+++ utilities/pickN.ts
@@ -1,5 +1,3 @@
-import { assertEquals } from "@std/assert";
-
 export function* pickN<T>(items: T[], n: number): Generator<T[]> {
   if (n === 0) {
     yield [];
@@ -19,23 +17,3 @@ export function* pickN<T>(items: T[], n: number): Generator<T[]> {
     }
   }
 }
-
-Deno.test("pickN", () => {
-  assertEquals([...pickN([1, 2, 3, 4], 1)], [
-    [1],
-    [2],
-    [3],
-    [4],
-  ]);
-  assertEquals([...pickN([1, 2, 3, 4], 2)], [
-    [1, 2],
-    [1, 3],
-    [1, 4],
-    [2, 3],
-    [2, 4],
-    [3, 4],
-  ]);
-  assertEquals([...pickN([1, 2, 3, 4], 4)], [
-    [1, 2, 3, 4],
-  ]);
-});
