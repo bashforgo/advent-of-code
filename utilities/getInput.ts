eb297b0484e0424834fb370b7a7c422e3c446047
Bash Forgo <6547523+bashforgo@users.noreply.github.com>
2025-02-11 20:55:32 +0000

    cache inputs

--- utilities/getInput.ts
+++ utilities/getInput.ts
@@ -1,7 +1,41 @@
 import "@std/dotenv";
+import { dirname } from "@std/path";
 import { fetchAdventOfCode } from "@utilities/fetchAdventOfCode.ts";
 
 export const getInput = async (year: number, day: number): Promise<string> => {
+  try {
+    return await readCache(year, day);
+  } catch {
+    // ignored
+  }
+
+  const input = await fetchInput(year, day);
+
+  try {
+    await writeCache(year, day, input);
+  } catch (error) {
+    console.error("Failed to write cache:", error);
+  }
+
+  return input;
+};
+
+const fetchInput = async (year: number, day: number) => {
   const response = await fetchAdventOfCode(`/${year}/day/${day}/input`);
   return await response.text();
+};
+
+const getCachePath = (year: number, day: number): string => {
+  const zeroPaddedDay = String(day).padStart(2, "0");
+  return `.cache/${year}-${zeroPaddedDay}.txt`;
+};
+
+const readCache = async (year: number, day: number) => {
+  return await Deno.readTextFile(getCachePath(year, day));
+};
+
+const writeCache = async (year: number, day: number, input: string) => {
+  const path = getCachePath(year, day);
+  await Deno.mkdir(dirname(path), { recursive: true });
+  await Deno.writeTextFile(path, input);
 };
