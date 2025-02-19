febab01139d7de19e547f7a9513d080bed93fc98
Bash Forgo <6547523+bashforgo@users.noreply.github.com>
2025-02-19 14:28:50 +0000

    2017 day 2

---
+++ 2017/02/main.ts
@@ -0,0 +1,41 @@
+import { maxOf, minOf, sumOf } from "@std/collections";
+import { ascend } from "@std/data-structures";
+import { getInput } from "@utilities/getInput.ts";
+import { pickN } from "@utilities/pickN.ts";
+
+const DEBUG = false;
+const input = DEBUG
+  ? `\
+5 9 2 8
+9 4 7 3
+3 8 6 5
+`
+  : await getInput(2017, 2);
+
+const lines = input.trim().split("\n");
+const cells = lines.map((line) => line.split(/\s+/).map((c) => Number(c)));
+
+const part1 = () => {
+  return sumOf(
+    cells,
+    (row) => {
+      const min = minOf(row, (x) => x)!;
+      const max = maxOf(row, (x) => x)!;
+      return max - min;
+    },
+  );
+};
+console.log(part1());
+
+const part2 = () => {
+  return sumOf(
+    cells,
+    (row) => {
+      const [low, high] = pickN(row.toSorted(ascend), 2).find(([low, high]) =>
+        high % low === 0
+      )!;
+      return high / low;
+    },
+  );
+};
+console.log(part2());
