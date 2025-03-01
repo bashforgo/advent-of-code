69020214e24373a25d8a4803d8187de6b9278d9d
Bash Forgo <6547523+bashforgo@users.noreply.github.com>
2025-03-01 20:07:50 +0000

    2018 day 6

---
+++ 2018/06/main.ts
@@ -0,0 +1,126 @@
+import { maxOf, minBy, minOf, sumOf } from "@std/collections";
+import { getInput } from "@utilities/getInput.ts";
+import { getAdjacentPoints } from "@utilities/grid/getAdjacentPoints.ts";
+import { getManhattanDistance } from "@utilities/grid/getManhattanDistance.ts";
+import { isInBoundsOfRectangle } from "@utilities/grid/isInBounds.ts";
+import { Point, point } from "@utilities/grid/Point.ts";
+import { Rectangle } from "@utilities/grid/Rectangle.ts";
+import { identity } from "@utilities/identity.ts";
+import { ObjectMap } from "@utilities/ObjectMap.ts";
+import { ObjectSet } from "@utilities/ObjectSet.ts";
+
+const DEBUG = false;
+const input = DEBUG
+  ? `\
+1, 1
+1, 6
+8, 3
+3, 4
+5, 5
+8, 9
+`
+  : await getInput(2018, 6);
+
+const points = ObjectSet.from(
+  input
+    .trim()
+    .split("\n")
+    .map((line): Point => {
+      const [x, y] = line.split(", ").map(Number);
+      return { x, y };
+    }),
+);
+
+const minX = minOf(points, (p) => p.x)! - 1;
+const minY = minOf(points, (p) => p.y)! - 1;
+const maxX = maxOf(points, (p) => p.x)! + 1;
+const maxY = maxOf(points, (p) => p.y)! + 1;
+const width = maxX - minX + 1;
+const height = maxY - minY + 1;
+
+const getClosestPoints = (from: Point) => {
+  const pointsByDistance = Map.groupBy(
+    points,
+    (to) => getManhattanDistance(from, to),
+  );
+  const minDistance = minBy(pointsByDistance.keys(), identity)!;
+  return pointsByDistance.get(minDistance)!;
+};
+
+const floodFill = (
+  bounds: Rectangle,
+  from: Point,
+  predicate?: (point: Point) => boolean,
+): ObjectSet<Point> => {
+  const filled = new ObjectSet<Point>();
+
+  const stack = [from];
+  while (stack.length > 0) {
+    const point = stack.pop()!;
+
+    const shouldFill = predicate?.(point) ?? true;
+    if (!shouldFill) continue;
+
+    if (filled.has(point)) continue;
+
+    filled.add(point);
+
+    stack.push(
+      ...getAdjacentPoints(point)
+        .filter((p) => isInBoundsOfRectangle(bounds, p)),
+    );
+  }
+
+  return filled;
+};
+
+const isPerimeter = (point: Point) =>
+  point.x === minX || point.x === maxX || point.y === minY || point.y === maxY;
+
+const part1 = () => {
+  const bounds = { x: minX, y: minY, width, height };
+
+  const floodFillPredicate = (from: Point) => (to: Point) => {
+    const closest = getClosestPoints(to);
+    if (closest.length !== 1) return false;
+
+    const [closestPoint] = closest;
+    return closestPoint === from;
+  };
+  const floodFillFrom = (from: Point) =>
+    floodFill(bounds, from, floodFillPredicate(from));
+
+  const areas = ObjectMap.from(
+    points
+      .values()
+      .map((from) => [from, floodFillFrom(from)] as const)
+      .filter(([, area]) => !area.values().some((p) => isPerimeter(p))),
+  );
+
+  return maxOf(areas.values(), (area) => area.size);
+};
+console.log(part1());
+
+const part2 = () => {
+  const maxDistance = DEBUG ? 32 : 10000;
+
+  const bounds = { x: minX, y: minY, width, height };
+
+  const floodFillPredicate = (to: Point) => {
+    const distance = sumOf(points, (from) => getManhattanDistance(from, to));
+    return distance < maxDistance;
+  };
+
+  const floodFillFrom = (from: Point) =>
+    floodFill(bounds, from, floodFillPredicate);
+
+  const center = point(
+    Math.round((minX + maxX) / 2),
+    Math.round((minY + maxY) / 2),
+  );
+
+  const area = floodFillFrom(center);
+
+  return area.size;
+};
+console.log(part2());
--- utilities/grid/isInBounds.ts
+++ utilities/grid/isInBounds.ts
@@ -1,5 +1,6 @@
 import { Grid } from "@utilities/grid/Grid.ts";
 import { Point } from "@utilities/grid/Point.ts";
+import { Rectangle } from "@utilities/grid/Rectangle.ts";
 
 export const isInBounds = <T>(grid: Grid<T>, { x, y }: Point): boolean =>
   isInBoundsRaw(grid[0].length, grid.length, { x, y });
@@ -9,3 +10,15 @@ export const isInBoundsRaw = (
   height: number,
   { x, y }: Point,
 ): boolean => y >= 0 && y < height && x >= 0 && x < width;
+
+export const isInBoundsOfRectangle = (
+  bounds: Rectangle,
+  { x, y }: Point,
+) => {
+  const { x: minX, y: minY, width, height } = bounds;
+  if (x < minX) return false;
+  if (y < minY) return false;
+  if (x >= minX + width) return false;
+  if (y >= minY + height) return false;
+  return true;
+};
